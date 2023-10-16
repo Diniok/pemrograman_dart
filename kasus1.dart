b7ba2d9eda0add2bf0280b853d65e408ee79846d
Diniok <141692293+Diniok@users.noreply.github.com>
2023-10-16 12:36:02 +0700

    Add files via upload

---
+++ kasus1.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main(){
+  stdout.write('masukan angka');
+  int angka= int.parse(stdin.readLineSync()!);
+
+  if(angka > 0){
+    print('positif');
+  }
+  else if(angka == 0){
+    print('nol');
+  }
+  else{
+    print('negatif');
+  }
+}
---
+++ kasus4.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+void main(){
+  var angka=1;
+
+  for (; angka <= 5;){
+    print('SAYA PASTI BISA,GASSKEUN...');
+    angka++;
+  }
+}
